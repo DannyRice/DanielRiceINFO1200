d38f649a4c07e9d6ad68e7f875ca0904f0e50cef
Daniel Rice <10921655@uvu.edu>
2023-02-02 10:17:06 -0700

    Participation 2 Assignment
    The first python assignment

---
+++ Participation 2/daniel.py
@@ -0,0 +1,45 @@
+print("Hello! Welcome to my first Python Assignment")  # Welcome Message
+print()  # makes a space
+print("My name is Daniel Rice")  # Shows my full name
+
+# Shows what specific class I am in
+print("I'm a student in Dr. Crandall's class: Spring Semester, Section 001")
+print()  # makes a space
+print("Fun Math Problems")  # shows the math problems section
+print("Rolling a 5 and a 4 on a pair of dice adds up to...")  # First math problem
+dice1 = 5  # creates first dice number variable
+dice2 = 4  # creates second dice number variable
+dice1+dice2  # the math equation
+print("9")  # the answer to the math problem
+print()  # makes a space
+
+# Second math problem
+print("There is 5 more days to turn an assignment in. If I do it in 2 days, I have how many days to start the next assignment?")
+due = 5  # creates due date variable
+do = 2  # creates variable of how many days it will be done in
+due-do  # the math equation
+print("3 days")  # print the answer
+print()  # makes a space
+
+# Third math problem
+print("If I have 3 hens that lay 2 eggs a day, how many eggs will I have in 4 days?")
+hens = 3  # number of hens variable
+eggs = 2  # numer of eggs variable
+days = 4  # number of days variable
+hens+eggs*days  # math equation
+print("11 Eggs")  # answer to the math problem
+print()  # makes a space
+
+# Fourth math problem
+print("I have completed 2 out of 25 assignments so far this semester...")
+done = 2  # number of assingments completed
+notdone = 25  # number of assingments left
+done/notdone  # math equation
+print("0.08 or 8%")  # answer to the math problem
+print()  # makes a space
+print("I went to the store and first found 3 items I wanted to buy for $4 each. Additionally, they had a .5 off coupon attached...")  # Fifth math problem
+Item = 3  # Number of items variable
+cost = 4  # cost of items variable
+coupon = 0.5  # discount variable
+Item*cost*coupon  # math equation
+print("$6")  # asnwer to the math problem
